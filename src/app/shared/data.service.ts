7edabb8bb46da07ace60fca045b840b02dc08ff5
Dyan Jessica M. Lim <dyan.lim@smprime.sm.ph>
2019-07-18 18:59:05 +0800

    created dark and light theme

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -17,61 +17,61 @@ export class DataService {
             id: 1,
             name: "Item 1",
             description: "Description for Item 1",
-            image: "https://placekitten.com/400/400"
+            image: "https://picsum.photos/id/343/400/400"
         },
         {
             id: 2,
             name: "Item 2",
             description: "Description for Item 2",
-            image: "https://placekitten.com/260/260"
+            image: "https://picsum.photos/id/383/400/400"
         },
         {
             id: 3,
             name: "Item 3",
             description: "Description for Item 3",
-            image: "https://placekitten.com/200/200"
+            image: "https://picsum.photos/id/353/400/400"
         },
         {
             id: 4,
             name: "Item 4",
             description: "Description for Item 4",
-            image: "https://placekitten.com/300/200"
+            image: "https://picsum.photos/id/363/400/400"
         },
         {
             id: 5,
             name: "Item 5",
             description: "Description for Item 5",
-            image: "https://placekitten.com/300/300"
+            image: "https://picsum.photos/id/373/400/400"
         },
         {
             id: 6,
             name: "Item 6",
             description: "Description for Item 6",
-            image: "https://placekitten.com/350/350"
+            image: "https://picsum.photos/id/383/400/400"
         },
         {
             id: 7,
             name: "Item 7",
             description: "Description for Item 7",
-            image: "https://placekitten.com/500/200"
+            image: "https://picsum.photos/id/393/400/400"
         },
         {
             id: 8,
             name: "Item 8",
             description: "Description for Item 8",
-            image: "https://placekitten.com/400/500"
+            image: "https://picsum.photos/id/313/400/400"
         },
         {
             id: 9,
             name: "Item 9",
             description: "Description for Item 9",
-            image: "https://placekitten.com/300/400"
+            image: "https://picsum.photos/id/323/400/400"
         },
         {
             id: 10,
             name: "Item 10",
             description: "Description for Item 10",
-            image: "https://placekitten.com/250/250"
+            image: "https://picsum.photos/id/333/400/400"
         },
        
     );
