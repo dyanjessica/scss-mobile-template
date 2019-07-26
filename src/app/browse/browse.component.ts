a5cca2828a7f374a75598346ab45c746fb5b5ff3
Dyan Jessica M. Lim <dyan.lim@smprime.sm.ph>
2019-07-26 17:06:31 +0800

    moonraft design concept

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -8,18 +8,18 @@ import { Component, OnInit } from "@angular/core";
 export class BrowseComponent implements OnInit {
     
     locations: { city: string, country: string, imageSrc: string }[] = [
-        { city: "Yarlford", country: "Aldorria", imageSrc: "https://placem.at/places?random=1&w=500&txt=0" },
-        { city: "Paentmarwy", country: "Bahari", imageSrc: "https://placem.at/places?random=2&w=500&txt=0" },
-        { city: "Landow", country: "Erewhon", imageSrc: "https://placem.at/places?random=3&w=500&txt=0" },
-        { city: "Penrith", country: "Gilead", imageSrc: "https://placem.at/places?random=4&w=500&txt=0" },
-        { city: "Aberystwyth", country: "Mandorra", imageSrc: "https://placem.at/places?random=5&w=500&txt=0" },
-        { city: "Macclesfield", country: "Nambutu", imageSrc: "https://placem.at/places?random=6&w=500&txt=0" },
-        { city: "Larnwick", country: "Deltora", imageSrc: "https://placem.at/places?random=77&w=500&txt=0" },
-        { city: "Snowbush", country: "Islandia", imageSrc: "https://placem.at/places?random=8&w=500&txt=0" },
-        { city: "Kelna", country: "Norteguay", imageSrc: "https://placem.at/places?random=55&w=500&txt=0" },
-        { city: "Drumnacanvy", country: "Graznavia", imageSrc: "https://placem.at/places?random=44&w=500&txt=0" },
-        { city: "Hartlepool", country: "Drasselstein", imageSrc: "https://placem.at/places?random=11&w=500&txt=0" },
-        { city: "Timeston", country: "Brungaria", imageSrc: "https://placem.at/places?random=33&w=500&txt=0" }
+        { city: "SM City Baguio", country: "Metro Manila", imageSrc: "https://placem.at/places?random=1&w=500&txt=0" },
+        { city: "SM City Bacoor", country: "South Luzon", imageSrc: "https://placem.at/places?random=2&w=500&txt=0" },
+        { city: "SM Aura Premier", country: "Metro Manila", imageSrc: "https://placem.at/places?random=3&w=500&txt=0" },
+        { city: "SM City Bicutan", country: "Metro Manila", imageSrc: "https://placem.at/places?random=4&w=500&txt=0" },
+        { city: "SM City Pampanga", country: "North Luzon", imageSrc: "https://placem.at/places?random=5&w=500&txt=0" },
+        { city: "SM Mall of Asia", country: "Metro Manila", imageSrc: "https://placem.at/places?random=6&w=500&txt=0" },
+        { city: "SM City Naga", country: "South Luzon", imageSrc: "https://placem.at/places?random=77&w=500&txt=0" },
+        { city: "SM City Cebu", country: "Visayas", imageSrc: "https://placem.at/places?random=8&w=500&txt=0" },
+        { city: "SM City Bacolod", country: "Visayas", imageSrc: "https://placem.at/places?random=55&w=500&txt=0" },
+        { city: "SM City Davao", country: "Mindanao", imageSrc: "https://placem.at/places?random=44&w=500&txt=0" },
+        { city: "SM City Batangas", country: "South Luzon", imageSrc: "https://placem.at/places?random=11&w=500&txt=0" },
+        { city: "SM City Iloilo", country: "Visayas", imageSrc: "https://placem.at/places?random=33&w=500&txt=0" }
     ];
     constructor() {
         // Use the component constructor to inject providers.
